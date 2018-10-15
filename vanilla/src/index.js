7223480ed353aa2eaa7ca258edf11cc267d883f5
Sam <samuelechu@umail.ucsb.edu>
2018-10-15 14:59:59 -0700

    store toDos in localStorage

--- vanilla/src/index.js
+++ vanilla/src/index.js
@@ -1,1 +1,34 @@
 import "./index.css";
+
+
+
+const addToDo = () => {
+    const newToDo = document.getElementById("toDoInput").value
+    const currentToDos = JSON.parse(localStorage.getItem("currentToDos"))
+    const newToDos = [...currentToDos, newToDo]
+    localStorage.setItem("currentToDos", JSON.stringify(newToDos))
+}
+
+const updateList = () => {
+    const currentToDos = JSON.parse(localStorage.getItem("currentToDos"))
+
+    const str = `<ul> 
+        ${currentToDos.map((toDo) => '<li>'+ toDo + '</li>').join("")}
+    </ul>`
+  
+    document.getElementById("currentToDos").innerHTML = str;
+}
+
+const processAdd = () => {
+    addToDo()
+    updateList()
+    
+}
+
+document.getElementById("addButton").addEventListener("click", processAdd);
+
+if (localStorage.getItem("currentToDos") == undefined){
+    localStorage.setItem("currentToDos", JSON.stringify([]) )
+} else {
+    updateList()
+}
